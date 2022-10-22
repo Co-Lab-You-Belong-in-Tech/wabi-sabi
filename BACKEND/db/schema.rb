98fb0fdb7074d4999ae461e6b14e1ef4b4a95f9b
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-10-22 11:13:19 +0100

    use uuid for user id

--- BACKEND/db/schema.rb
+++ BACKEND/db/schema.rb
@@ -44,13 +44,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_194532) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "api_v1_memories", force: :cascade do |t|
+  create_table "api_v1_memories", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "prompt"
     t.text "story"
     t.boolean "public"
     t.boolean "favorite"
-    t.uuid "share_id", default: -> { "gen_random_uuid()" }
-    t.bigint "user_id", null: false
+    t.uuid "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_api_v1_memories_on_user_id"
@@ -64,7 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_194532) do
     t.index ["jti"], name: "index_jwt_denylist_on_jti"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
