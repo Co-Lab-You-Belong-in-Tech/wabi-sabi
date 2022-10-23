08d20702fc44b01c2ab88cfdf7a053c482bc75f5
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-10-23 14:04:04 +0100

    refactor home page

--- FRONTEND/src/pages/home.js
+++ FRONTEND/src/pages/home.js
@@ -1,37 +1,32 @@
-/* eslint-disable react/no-array-index-key */
-/* eslint-disable react/prop-types */
-// eslint-disable-next-line import/no-duplicates
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect, useRef } from 'react';
+import moment from 'moment/moment';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css/pagination';
-import { Pagination } from 'swiper';
 import { useRouter } from 'next/router';
 
 // Import Swiper styles
 import 'swiper/css';
 import { useSelector } from 'react-redux';
 import AppLayout from '../components/Layouts/AppLayout';
-import AuthLayout from '../components/Layouts/AuthLayout';
 
-const Questions = [
-  'What’s one thing you are grateful for today?',
-  'What is the highlight of your day?',
-  'What makes you smile today?',
+const questions = [
+  {
+    question: 'What’s one thing you are grateful for today?',
+    id: 1,
+  },
+  {
+    question: 'What is the highlight of your day?',
+    id: 2,
+  },
+  {
+    question: 'What makes you smile today?',
+    id: 3,
+  }
 ];
 
-const today = new Date();
-const options = {
-  month: 'short',
-};
-const month = today.toLocaleString('en-GB', options).toUpperCase();
+const currentDate = moment().format('ll').split(',').join('');
 
-const day = today.getDate();
-
-const year = today.getFullYear();
-
-const currentDate = `${month} ${day} ${year}`;
 
 const breakpoints = {
   0: {
@@ -48,11 +43,11 @@ const breakpoints = {
   },
 };
 
-const LandingPage = () => {
+function LandingPage() {
   const router = useRouter();
 
-  const isLoggedIn = useSelector((state) => state.account.isLoggedIn);
-
+  // const isLoggedIn = useSelector((state) => state.account.isLoggedIn);
+  const isLoggedIn = true
   useEffect(() => {
     if (isLoggedIn === false) {
       router.push('/account/login');
@@ -66,67 +61,75 @@ const LandingPage = () => {
     setActiveIndex(id);
   };
 
-  return (
-    <AuthLayout>
-      <AppLayout>
-        {/* <Header /> */}
+  // change the swiper slide centered property when the domwidth changes
+  const [domWidth, setDomWidth] = useState(0);
+  const pageRef = useRef(null);
+  const handleResize = () => {
+    setDomWidth(pageRef.current.offsetWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", handleResize)
 
-        <main className="bg-white w-full h-[823px] overflow-hidden text-center text-[25px] text-white font-roboto">
-          <div className="shadow-3xl hidden md:block text-2xl p-2 pl-6 pr-6 bg-white w-fit rounded-[15px] text-black m-auto mt-[102px] mb-0">
-            <p className="tracking-[0.02em] leading-5 mx-3">
-              Choose a question to start creating a memory.
-            </p>
-          </div>
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    }
+  }, [pageRef.current]);
+
+  return (
+    <AppLayout>
+      <main className="flex flex-col items-center justify-center w-full h-screen overflow-hidden text-4xl text-center text-white bg-white font-roboto gap-y-16" ref={pageRef}>
+        <div className="hidden p-2 px-6 text-2xl text-black bg-white shadow-3xl md:block w-fit rounded-2xl">
+          <p className="tracking-[0.02em] leading-5">
+            Choose a question to start creating a memory.
+          </p>
+        </div>
+        <div className='mx-16 md:max-w-max'>
           <Swiper
-            className="mySwiper absolute mr-5 top-[100px] h-[355px] m-auto !pr-[40vw] items-center !overflow-visible"
+            className="mySwiper h-[355px] !overflow-visible"
             slidesPerView="auto"
             spaceBetween={36}
-            centeredSlides
-            breakpoints={breakpoints}
-            pagination={{
-              dynamicBullets: true,
-              clickable: true,
-            }}
-            modules={[Pagination]}
+            centeredSlides={domWidth < 900}
           >
-            {Questions.map((question, index) => (
+            {questions.map((item) => (
               <SwiperSlide
-                className="absolute top-[0px] items-center left-[0px] !w-[242px] rounded-[20px] shadow-3xl  h-[355px]"
-                key={index}
+                className="items-center !w-60 rounded-[20px] shadow-3xl h-[355px]"
+                key={item.id}
               >
                 <Slide
-                  question={question}
+                  question={item.question}
                   handleClick={handleClick}
                   activeIndex={activeIndex}
-                  index={index}
+                  index={item.id}
                 />
               </SwiperSlide>
             ))}
           </Swiper>
-        </main>
-      </AppLayout>
-    </AuthLayout>
+        </div>
+      </main>
+    </AppLayout>
   );
 };
 
 export default LandingPage;
 
-function Slide({ question, handleClick, activeIndex, index }) {
+function Slide({
+  question, handleClick, activeIndex, index,
+}) {
   return (
-    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
-    <div
+    <button
       onClick={() => handleClick(index)}
-      className={`w-full h-full rounded-[20px] transition-opacity duration-300 ease-out ${
-        activeIndex === index ? 'bg-home-card opacity-60' : 'bg-home-card'
-      }`}
+      className={`h-full rounded-[20px] transition-opacity duration-300 ease-out relative p-5 w-60  ${activeIndex === index ? 'bg-home-card opacity-60' : 'bg-home-card'
+        }`}
     >
-      <strong className="absolute top-[112px] left-[33px] tracking-[0.02em] inline-block w-[181px] h-[116px]">
+      <strong className="tracking-wide inline-block w-[181px]">
         {question}
-      </strong>{' '}
-      <div className="absolute top-[303.5px] left-[19.5px] border-t-[3px_solid_#dbdadb] bg-green-100 box-border w-[84px] h-[3px]" />
-      <div className="absolute top-[300px] left-[111px] text-[12px] tracking-[0.02em] inline-block w-[73px] h-[12px]">
-        {currentDate}
+      </strong>
+      <div className='absolute flex items-center bottom-5 gap-x-4'>
+        <div className=" bg-white w-[84px] h-1" />
+        <div className="text-base tracking-wide uppercase">
+          {currentDate}
+        </div>
       </div>
-    </div>
+    </button>
   );
 }
