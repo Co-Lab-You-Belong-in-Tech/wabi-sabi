3e1f200bec3071cd5f4ea4abd4a2ba4603b81e57
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-10-23 13:06:34 +0100

    reset config settings

--- FRONTEND/tailwind.config.js
+++ FRONTEND/tailwind.config.js
@@ -5,40 +5,13 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    screens: {
-      ss: '320px',
-      sm: '640px',
-      // => @media (min-width: 640px) { ... }
-
-      md: '768px',
-      // => @media (min-width: 768px) { ... }
-
-      lg: '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      xl: '1280px',
-      // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
-    },
     extend: {
       fontFamily: {
         montserrat: 'Montserrat',
-        roboto: 'Roboto',
+        roboto: ['Roboto', 'sans-serif'],
         ANLP: 'Avenir Next LT Pro',
       },
       colors: {
-        green: {
-          100: '#f7f7f9',
-          200: '#37C367',
-          300: '#747373',
-          400: '#0d151e',
-          500: '#949495',
-          600: '#9C9B9B',
-          700: '#344563',
-          800: '#37C36760',
-        },
         white: '#fff',
         black: '#000',
         profile: '#F9A566',
@@ -49,6 +22,7 @@ module.exports = {
       },
       boxShadow: {
         '3xl': '0px 8px 24px rgba(0, 0, 0, 0.15)',
+        'box': '0px 8px 24px rgba(0, 0, 0, 0.15)',
       },
       borderRadius: {
         '4xl': '0px 0px 15px 15px',
@@ -60,16 +34,14 @@ module.exports = {
         'home-card':
           'linear-gradient(172.93deg, #348D52 32.33%, #348D5200 72.2%), linear-gradient(#37C367, #37C367)',
       },
-      boxShadow: {
-        box: '0px 8px 24px rgba(0, 0, 0, 0.15)',
-      },
       fontSize: {
         base: '12px',
         lg: '14px',
         xl: '15px',
         '2xl': '18px',
         '3xl': '20px',
-        '4xl': '35px',
+        '4xl': '25px',
+        '5xl': '35px',
       },
       animation: {
         shake: 'shake 0.82s cubic-bezier(.36,.07,.19,.97) both',
@@ -84,5 +56,4 @@ module.exports = {
       },
     },
   },
-  corePlugins: { preflight: false },
 };
