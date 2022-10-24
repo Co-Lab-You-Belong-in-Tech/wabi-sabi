76455bf077144cc2a0cd48f9a5ec314de4d8cf06
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-10-25 00:42:16 +0100

    desktop onboarding page

--- FRONTEND/src/components/DesktopHeader.js
+++ FRONTEND/src/components/DesktopHeader.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types'
 
 function DesktopHeader({ children }) {
   return (
-    <header className="flex items-center justify-between w-full bg-white h-[72px] px-36 drop-shadow-3xl">
+    <header className="flex items-center justify-between w-full bg-white h-[72px] px-36 drop-shadow-3xl fixed top-0">
       <Image src="/assets/Logo.svg" alt="Logo" width={144} height={63} />
       {children}
     </header>
