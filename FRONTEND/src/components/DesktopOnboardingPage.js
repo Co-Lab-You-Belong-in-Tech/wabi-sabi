b60ccdf21f044b8596229010f631dc4734bf4e04
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-10-24 21:43:30 +0100

    switch onboarding page on resize

--- FRONTEND/src/components/DesktopOnboardingPage.js
+++ FRONTEND/src/components/DesktopOnboardingPage.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import Link from 'next/link';
+import DesktopHeader from './DesktopHeader';
 
 function DesktopOnboardingPage() {
   return (
     <>
+      <DesktopHeader>
+        <nav className="flex items-center gap-x-2">
+          <Link href="/account/register">
+            <p className='text-xl font-bold px-3 py-[10px] cursor-pointer'>Sign up</p>
+          </Link>
+          <Link href="/account/login">
+            <p className='text-xl font-bold px-3 py-[10px] cursor-pointer'>Log in</p>
+          </Link>
+        </nav>
+      </DesktopHeader>
     </>
   );
 }
--- FRONTEND/src/pages/index.js
+++ FRONTEND/src/pages/index.js
@@ -1,22 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, useLayoutEffect } from 'react';
 import MobileOnboardingPage from '../components/MobileOnboardingPage';
 import DesktopOnboardingPage from '../components/DesktopOnboardingPage';
 
 function Onboarding() {
   const [isMobile, setIsMobile] = useState(false);
+  const [domWidth, setDomWidth] = useState(0);
+
+  const pageRef = useRef(null);
 
   useEffect(() => {
-    if (window.innerWidth < 768) {
+    if (domWidth < 768) {
       setIsMobile(true);
     } else {
       setIsMobile(false);
     }
+  }, [domWidth]);
+  
+  const handleResize = () => {
+    setDomWidth(pageRef.current.offsetWidth);
+  };
+
+  useLayoutEffect(() => {
+    handleResize();
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
   }, []);
 
   return (
-    <>
+    <div ref={pageRef}>
       {isMobile ? <MobileOnboardingPage /> : <DesktopOnboardingPage />}
-    </>
+    </div>
   );
 }
 
