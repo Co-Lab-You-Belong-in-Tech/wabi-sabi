d2819d3cb7ff590e196f29eb0d3af050bebeaa11
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-10-30 23:53:45 +0100

    delete user dependents

--- BACKEND/app/models/user.rb
+++ BACKEND/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :api_v1_memories, class_name: 'Api::V1::Memory', foreign_key: 'user_id'
+  has_many :api_v1_memories, class_name: 'Api::V1::Memory', foreign_key: 'user_id', dependent: :destroy
 
   devise :database_authenticatable,
          :jwt_authenticatable,
