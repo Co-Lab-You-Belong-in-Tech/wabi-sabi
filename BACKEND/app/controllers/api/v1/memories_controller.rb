64b116de912bdafc09eb6d2bce8e8a628b746991
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-10-31 01:43:07 +0100

    fix rubocop issues

--- BACKEND/app/controllers/api/v1/memories_controller.rb
+++ BACKEND/app/controllers/api/v1/memories_controller.rb
@@ -7,18 +7,17 @@ class Api::V1::MemoriesController < ApplicationController
     @api_v1_memories = Api::V1::Memory.includes(:image_attachment).where(user: current_user).order(created_at: :desc)
     memories_data = {}
     @api_v1_memories.each do |memory|
-      date = memory.created_at.strftime("%Y %B")
+      date = memory.created_at.strftime('%Y %B')
       memories_data[date] = [] unless memories_data[date]
       memories_data[date] << Api::V1::MemorySerializer.new(memory).serializable_hash[:data][:attributes]
     end
 
     data = []
     memories_data.each do |key, value|
-      data << {month: key, memories: value}
+      data << { month: key, memories: value }
     end
     render json: data
   end
-
 
   # GET /api/v1/memories/public
   def public_memories
