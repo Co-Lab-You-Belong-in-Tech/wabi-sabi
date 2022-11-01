69eafbe95edae9763c495f688289a812bc4ff26a
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-11-01 08:25:20 +0100

    discover icon

--- FRONTEND/src/components/navItems.js
+++ FRONTEND/src/components/navItems.js
@@ -1,20 +1,17 @@
-import InActiveCompas from '../../public/assets/Compass.svg';
-import ActiveCompas from '../../public/assets/CompassColored.svg';
-
 const navItems = [
+   {
+    inactive: '/assets/inactive-home-icon.svg',
+    active: '/assets/active-home-icon.svg',
+    url: '/home',
+  },
   {
     inactive: '/assets/inactive-memories-icon.svg',
     active: '/assets/active-memories-icon.svg',
     url: '/memories',
   },
   {
-    inactive: '/assets/inactive-home-icon.svg',
-    active: '/assets/active-home-icon.svg',
-    url: '/home',
-  },
-  {
-    inactive: InActiveCompas,
-    active: ActiveCompas,
+    inactive: '/assets/inactive-public.svg',
+    active: '/assets/active-public.svg',
 
     url: '/discover',
   },
