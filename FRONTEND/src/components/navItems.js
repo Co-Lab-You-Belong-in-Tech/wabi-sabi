20ddc9fcb1cc9ffdb719b3eb71ef95b645bdf01f
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-11-01 12:21:13 +0100

    use single nav items

--- FRONTEND/src/components/navItems.js
+++ FRONTEND/src/components/navItems.js
@@ -1,13 +1,13 @@
 const navItems = [
-   {
-    inactive: '/assets/inactive-home-icon.svg',
-    active: '/assets/active-home-icon.svg',
-    url: '/home',
-  },
   {
     inactive: '/assets/inactive-memories-icon.svg',
     active: '/assets/active-memories-icon.svg',
     url: '/memories',
+  },
+  {
+    inactive: '/assets/inactive-home-icon.svg',
+    active: '/assets/active-home-icon.svg',
+    url: '/home',
   },
   {
     inactive: '/assets/inactive-public.svg',
