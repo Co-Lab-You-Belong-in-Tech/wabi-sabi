0cd56ee317587b87bad4dcafa6861155aa8e18e6
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-11-04 01:37:44 +0100

    add model validations

--- BACKEND/app/models/user.rb
+++ BACKEND/app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_many :api_v1_memories, class_name: 'Api::V1::Memory', foreign_key: 'user_id', dependent: :destroy
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :name, presence: true
 
   devise :database_authenticatable,
          :jwt_authenticatable,
