876b720d45ff8e6468b9b5ae38a89e8d22ddfda8
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-11-05 11:00:12 +0100

    use uuid as default orm_id

--- BACKEND/config/application.rb
+++ BACKEND/config/application.rb
@@ -23,5 +23,10 @@ module WabiSabiApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # setup UUIDs for all models
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
