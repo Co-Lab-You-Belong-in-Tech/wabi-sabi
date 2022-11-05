c8ec960ff9b10ac42273ae32f052e8d619f340c7
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-11-05 11:02:30 +0100

    regenrate models with new config

--- BACKEND/db/migrate/20221006221111_devise_create_users.rb
+++ BACKEND/db/migrate/20221006221111_devise_create_users.rb
@@ -2,7 +2,6 @@
 
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
-    enable_extension 'pgcrypto'
     create_table :users, id: :uuid, default: 'gen_random_uuid()' do |t|
       ## Database authenticatable
       t.string :name,               null: false, default: ""
--- BACKEND/db/migrate/20221011194532_create_api_v1_memories.rb
+++ BACKEND/db/migrate/20221011194532_create_api_v1_memories.rb
@@ -1,6 +1,5 @@
 class CreateApiV1Memories < ActiveRecord::Migration[7.0]
   def change
-    enable_extension 'pgcrypto'
     create_table :api_v1_memories, id: :uuid, default: 'gen_random_uuid()' do |t|
       t.string :prompt
       t.text :story
