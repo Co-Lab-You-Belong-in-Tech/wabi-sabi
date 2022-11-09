a63429309e0acb386b5f4de0038a1c797bce1a8e
OLUWANIFEMI <latunde.nifemi2016@gmail.com>
2022-11-09 12:06:38 +0100

    Fetching allmemories and public memories on login

--- FRONTEND/src/components/Layouts/AppLayout.jsx
+++ FRONTEND/src/components/Layouts/AppLayout.jsx
@@ -2,16 +2,21 @@
 /* eslint-disable react/default-props-match-prop-types */
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useRouter } from 'next/router';
 import { toast } from 'react-toastify';
 import DesktopHeader from '../DesktopHeader';
 import Footer from '../Footer';
+import { getAllMemories } from '../../redux/features/memory/memorySlice';
+import { getPublicMemories } from '../../redux/features/discover/discoverSlice';
 
 function AppLayout({ children, renderSide, renderNav }) {
-  const isLoggedIn = useSelector((state) => state.account.isLoggedIn);
+  const { isLoggedIn, register_success } = useSelector(
+    (state) => state.account
+  );
   const router = useRouter();
   const protectedRoutes = [
+    '/welcome',
     '/home',
     '/memories',
     '/discover',
@@ -21,7 +26,9 @@ function AppLayout({ children, renderSide, renderNav }) {
   ];
 
   useEffect(() => {
-    if (
+    if (isLoggedIn && register_success) {
+      router.push('/welcome');
+    } else if (
       isLoggedIn &&
       (router.pathname === '/account/login' ||
         router.pathname === '/account/register' ||
@@ -29,12 +36,24 @@ function AppLayout({ children, renderSide, renderNav }) {
     ) {
       router.push('/home');
     }
+  }, [isLoggedIn, register_success]);
 
+  useEffect(() => {
     if (!isLoggedIn && protectedRoutes.includes(router.pathname)) {
-      router.push('/account/login');
       toast.warning('You need to be logged in to access this page');
+      router.push('/account/login');
     }
   }, []);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      dispatch(getAllMemories());
+      dispatch(getPublicMemories());
+    }
+  }, [isLoggedIn]);
+
   return (
     <div className="font-roboto">
       <DesktopHeader renderSide={renderSide} />
